b534ec860b27eeb9abed230ea43b8473519e0711
Mushus <mushus.wynd@gmail.com>
2018-04-21 13:46:28 +0900

    GITHUB_TOKEN

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ git config user.name "Travis-CI"
 git init
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@github.com/Mushus/Mushus.github.io.git" master:master > /dev/null 2>&1
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/Mushus/Mushus.github.io.git" master:master > /dev/null 2>&1
